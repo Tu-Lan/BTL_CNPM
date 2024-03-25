0f6605d728b00a7ed4e50f8d323c1b45f2a1fb20
Tu-Lan <luuphuongvy0209@gmail.com>
2024-03-26 01:11:01 +0700

    complete form login signup not exist permission granted

--- CNPM/frmDangNhap.Designer.cs
+++ CNPM/frmDangNhap.Designer.cs
@@ -29,13 +29,176 @@ namespace CNPM
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnDangNhap = new System.Windows.Forms.Button();
+            this.checkBoxPass = new System.Windows.Forms.CheckBox();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.lbPass = new System.Windows.Forms.Label();
+            this.lbUser = new System.Windows.Forms.Label();
+            this.lbTitle = new System.Windows.Forms.Label();
+            this.sqlCommand1 = new Microsoft.Data.SqlClient.SqlCommand();
+            this.lbSignUp = new System.Windows.Forms.Label();
+            this.returnDangKy = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(660, 401);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(131, 55);
+            this.btnExit.TabIndex = 12;
+            this.btnExit.Text = "Thoát";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // btnDangNhap
+            // 
+            this.btnDangNhap.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDangNhap.Location = new System.Drawing.Point(387, 401);
+            this.btnDangNhap.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnDangNhap.Name = "btnDangNhap";
+            this.btnDangNhap.Size = new System.Drawing.Size(185, 55);
+            this.btnDangNhap.TabIndex = 13;
+            this.btnDangNhap.Text = "Đăng Nhập";
+            this.btnDangNhap.UseVisualStyleBackColor = true;
+            this.btnDangNhap.Click += new System.EventHandler(this.btnDangNhap_Click);
+            // 
+            // checkBoxPass
+            // 
+            this.checkBoxPass.AutoSize = true;
+            this.checkBoxPass.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBoxPass.Location = new System.Drawing.Point(799, 340);
+            this.checkBoxPass.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.checkBoxPass.Name = "checkBoxPass";
+            this.checkBoxPass.Size = new System.Drawing.Size(105, 38);
+            this.checkBoxPass.TabIndex = 11;
+            this.checkBoxPass.Text = "Show";
+            this.checkBoxPass.UseVisualStyleBackColor = true;
+            this.checkBoxPass.CheckedChanged += new System.EventHandler(this.checkBoxPass_CheckedChanged);
+            // 
+            // txtPass
+            // 
+            this.txtPass.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPass.Location = new System.Drawing.Point(387, 341);
+            this.txtPass.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtPass.Multiline = true;
+            this.txtPass.Name = "txtPass";
+            this.txtPass.PasswordChar = '*';
+            this.txtPass.Size = new System.Drawing.Size(403, 36);
+            this.txtPass.TabIndex = 9;
+            // 
+            // txtUser
+            // 
+            this.txtUser.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUser.Location = new System.Drawing.Point(387, 245);
+            this.txtUser.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtUser.Multiline = true;
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(403, 36);
+            this.txtUser.TabIndex = 10;
+            // 
+            // lbPass
+            // 
+            this.lbPass.AutoSize = true;
+            this.lbPass.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbPass.Location = new System.Drawing.Point(209, 345);
+            this.lbPass.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbPass.Name = "lbPass";
+            this.lbPass.Size = new System.Drawing.Size(136, 34);
+            this.lbPass.TabIndex = 7;
+            this.lbPass.Text = "Mật Khẩu";
+            // 
+            // lbUser
+            // 
+            this.lbUser.AutoSize = true;
+            this.lbUser.BackColor = System.Drawing.Color.Transparent;
+            this.lbUser.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbUser.Location = new System.Drawing.Point(209, 249);
+            this.lbUser.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbUser.Name = "lbUser";
+            this.lbUser.Size = new System.Drawing.Size(134, 34);
+            this.lbUser.TabIndex = 8;
+            this.lbUser.Text = "Tài khoản";
+            // 
+            // lbTitle
+            // 
+            this.lbTitle.AutoSize = true;
+            this.lbTitle.Font = new System.Drawing.Font("Times New Roman", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTitle.Location = new System.Drawing.Point(463, 119);
+            this.lbTitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbTitle.Name = "lbTitle";
+            this.lbTitle.Size = new System.Drawing.Size(233, 51);
+            this.lbTitle.TabIndex = 6;
+            this.lbTitle.Text = "Đăng Nhập";
+            // 
+            // sqlCommand1
+            // 
+            this.sqlCommand1.CommandTimeout = 30;
+            this.sqlCommand1.EnableOptimizedParameterBinding = false;
+            // 
+            // lbSignUp
+            // 
+            this.lbSignUp.AutoSize = true;
+            this.lbSignUp.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbSignUp.Location = new System.Drawing.Point(380, 473);
+            this.lbSignUp.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbSignUp.Name = "lbSignUp";
+            this.lbSignUp.Size = new System.Drawing.Size(249, 34);
+            this.lbSignUp.TabIndex = 14;
+            this.lbSignUp.Text = "Chưa có tài khoản!!";
+            // 
+            // returnDangKy
+            // 
+            this.returnDangKy.AutoSize = true;
+            this.returnDangKy.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.returnDangKy.Location = new System.Drawing.Point(643, 478);
+            this.returnDangKy.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.returnDangKy.Name = "returnDangKy";
+            this.returnDangKy.Size = new System.Drawing.Size(105, 31);
+            this.returnDangKy.TabIndex = 15;
+            this.returnDangKy.TabStop = true;
+            this.returnDangKy.Text = "Đăng ký";
+            this.returnDangKy.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.returnDangKy_LinkClicked);
+            // 
+            // frmDangNhap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1279, 710);
+            this.Controls.Add(this.returnDangKy);
+            this.Controls.Add(this.lbSignUp);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnDangNhap);
+            this.Controls.Add(this.checkBoxPass);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.lbPass);
+            this.Controls.Add(this.lbUser);
+            this.Controls.Add(this.lbTitle);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "frmDangNhap";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "Đăng Nhập";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmDangNhap_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnDangNhap;
+        private System.Windows.Forms.CheckBox checkBoxPass;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.Label lbPass;
+        private System.Windows.Forms.Label lbUser;
+        private System.Windows.Forms.Label lbTitle;
+        private Microsoft.Data.SqlClient.SqlCommand sqlCommand1;
+        private System.Windows.Forms.Label lbSignUp;
+        private System.Windows.Forms.LinkLabel returnDangKy;
     }
 }
 
--- CNPM/frmDangNhap.cs
+++ CNPM/frmDangNhap.cs
@@ -1,7 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -16,5 +18,75 @@ namespace CNPM
         {
             InitializeComponent();
         }
+        
+        private bool isUserWantsToExit = false;
+
+        private void frmDangNhap_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (!isUserWantsToExit)
+            {
+                if (MessageBox.Show("Bạn có muốn thoát chương trình?", "Thông Báo", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    isUserWantsToExit = true;
+                    Application.Exit();
+                }
+                else
+                {
+                    e.Cancel = true;
+                }
+            }
+        }
+
+        private void checkBoxPass_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBoxPass.Checked)
+            {
+                txtPass.UseSystemPasswordChar = true;
+            }
+            else
+            {
+                txtPass.UseSystemPasswordChar = false;
+            }
+        }
+        string constr = ConfigurationManager.ConnectionStrings["QLHS"].ConnectionString;
+        private void btnDangNhap_Click(object sender, EventArgs e)
+        {
+            string username = txtUser.Text.Trim();
+            string password = txtPass.Text.Trim();
+            if(username !="" && password != "")
+            {
+                string query = "SELECT COUNT(*) FROM tblSignUp WHERE sUserName='" + username + "' and " + "sPassword='" + password + "'";
+                using (SqlConnection connection = new SqlConnection(constr))
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand(query, connection);
+                    int v = (int)command.ExecuteScalar();
+                    if (v != 1)
+                    {
+                        MessageBox.Show("Tài khoản hoặc mật khẩu không đúng!", "Cảnh báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Chào mừng!!", "Thông báo");
+                        frmTrangChu trangChu = new frmTrangChu();
+                        this.Hide();
+                        trangChu.Show();
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Điền hết các ô đi!!");
+            }
+        }
+
+        private void returnDangKy_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            frmDangKy dangKy= new frmDangKy();
+            this.Hide();
+            dangKy.Show();
+        }
+
+
     }
 }
