b014d3b59ca5284fe431978ea2401e2c67cca549
mei932 <meiyi1010302@gmail.com>
2024-03-30 22:59:10 +0700

    frmGV

---
+++ CNPM/frmGiaoVien.Designer.cs
@@ -0,0 +1,293 @@
+﻿
+namespace CNPM
+{
+    partial class frmGiaoVien
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMaDT = new System.Windows.Forms.TextBox();
+            this.lblMaDanToc = new System.Windows.Forms.Label();
+            this.btnChiTiet = new System.Windows.Forms.Button();
+            this.lblMaHS = new System.Windows.Forms.Label();
+            this.txtTimKiem = new System.Windows.Forms.TextBox();
+            this.lbTimkiem = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.lblAddress = new System.Windows.Forms.Label();
+            this.lblGender = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblTenHS = new System.Windows.Forms.Label();
+            this.txtMaGV = new System.Windows.Forms.TextBox();
+            this.lbTitle = new System.Windows.Forms.Label();
+            this.dgvGV = new System.Windows.Forms.DataGridView();
+            this.cb_mmh = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvGV)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtMaDT
+            // 
+            this.txtMaDT.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMaDT.Location = new System.Drawing.Point(510, 71);
+            this.txtMaDT.Name = "txtMaDT";
+            this.txtMaDT.Size = new System.Drawing.Size(164, 26);
+            this.txtMaDT.TabIndex = 69;
+            // 
+            // lblMaDanToc
+            // 
+            this.lblMaDanToc.AutoSize = true;
+            this.lblMaDanToc.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMaDanToc.Location = new System.Drawing.Point(405, 118);
+            this.lblMaDanToc.Name = "lblMaDanToc";
+            this.lblMaDanToc.Size = new System.Drawing.Size(86, 19);
+            this.lblMaDanToc.TabIndex = 68;
+            this.lblMaDanToc.Text = "Mã môn học";
+            // 
+            // btnChiTiet
+            // 
+            this.btnChiTiet.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnChiTiet.Location = new System.Drawing.Point(689, 285);
+            this.btnChiTiet.Name = "btnChiTiet";
+            this.btnChiTiet.Size = new System.Drawing.Size(0, 0);
+            this.btnChiTiet.TabIndex = 67;
+            this.btnChiTiet.Text = "Chi tiết học sinh";
+            this.btnChiTiet.UseVisualStyleBackColor = true;
+            // 
+            // lblMaHS
+            // 
+            this.lblMaHS.AutoSize = true;
+            this.lblMaHS.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMaHS.Location = new System.Drawing.Point(50, 61);
+            this.lblMaHS.Name = "lblMaHS";
+            this.lblMaHS.Size = new System.Drawing.Size(87, 19);
+            this.lblMaHS.TabIndex = 66;
+            this.lblMaHS.Text = "Mã giáo viên";
+            // 
+            // txtTimKiem
+            // 
+            this.txtTimKiem.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTimKiem.Location = new System.Drawing.Point(143, 286);
+            this.txtTimKiem.Name = "txtTimKiem";
+            this.txtTimKiem.Size = new System.Drawing.Size(198, 26);
+            this.txtTimKiem.TabIndex = 65;
+            this.txtTimKiem.TextChanged += new System.EventHandler(this.txtTimKiem_TextChanged);
+            // 
+            // lbTimkiem
+            // 
+            this.lbTimkiem.AutoSize = true;
+            this.lbTimkiem.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTimkiem.Location = new System.Drawing.Point(49, 291);
+            this.lbTimkiem.Name = "lbTimkiem";
+            this.lbTimkiem.Size = new System.Drawing.Size(65, 19);
+            this.lbTimkiem.TabIndex = 64;
+            this.lbTimkiem.Text = "Tìm kiếm";
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.Location = new System.Drawing.Point(766, 287);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 26);
+            this.btnReset.TabIndex = 63;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnXoa.Location = new System.Drawing.Point(637, 287);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(75, 26);
+            this.btnXoa.TabIndex = 62;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // btnSua
+            // 
+            this.btnSua.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSua.Location = new System.Drawing.Point(510, 287);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(75, 26);
+            this.btnSua.TabIndex = 61;
+            this.btnSua.Text = "Sửa";
+            this.btnSua.UseVisualStyleBackColor = true;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(373, 287);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 26);
+            this.btnAdd.TabIndex = 60;
+            this.btnAdd.Text = "Thêm";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAddress.Location = new System.Drawing.Point(166, 161);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(164, 26);
+            this.txtAddress.TabIndex = 58;
+            // 
+            // lblAddress
+            // 
+            this.lblAddress.AutoSize = true;
+            this.lblAddress.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAddress.Location = new System.Drawing.Point(83, 166);
+            this.lblAddress.Name = "lblAddress";
+            this.lblAddress.Size = new System.Drawing.Size(54, 19);
+            this.lblAddress.TabIndex = 57;
+            this.lblAddress.Text = "Địa chỉ:";
+            // 
+            // lblGender
+            // 
+            this.lblGender.AutoSize = true;
+            this.lblGender.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGender.Location = new System.Drawing.Point(405, 71);
+            this.lblGender.Name = "lblGender";
+            this.lblGender.Size = new System.Drawing.Size(70, 19);
+            this.lblGender.TabIndex = 54;
+            this.lblGender.Text = "Điện thoại";
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.Location = new System.Drawing.Point(166, 111);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(164, 26);
+            this.txtName.TabIndex = 53;
+            // 
+            // lblTenHS
+            // 
+            this.lblTenHS.AutoSize = true;
+            this.lblTenHS.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTenHS.Location = new System.Drawing.Point(50, 118);
+            this.lblTenHS.Name = "lblTenHS";
+            this.lblTenHS.Size = new System.Drawing.Size(89, 19);
+            this.lblTenHS.TabIndex = 52;
+            this.lblTenHS.Text = "Tên giáo viên";
+            // 
+            // txtMaGV
+            // 
+            this.txtMaGV.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMaGV.Location = new System.Drawing.Point(166, 61);
+            this.txtMaGV.Name = "txtMaGV";
+            this.txtMaGV.Size = new System.Drawing.Size(164, 26);
+            this.txtMaGV.TabIndex = 51;
+            // 
+            // lbTitle
+            // 
+            this.lbTitle.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbTitle.Font = new System.Drawing.Font("Times New Roman", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTitle.Location = new System.Drawing.Point(403, 11);
+            this.lbTitle.Name = "lbTitle";
+            this.lbTitle.Size = new System.Drawing.Size(168, 29);
+            this.lbTitle.TabIndex = 50;
+            this.lbTitle.Text = "GIÁO VIÊN";
+            // 
+            // dgvGV
+            // 
+            this.dgvGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvGV.Location = new System.Drawing.Point(40, 344);
+            this.dgvGV.Name = "dgvGV";
+            this.dgvGV.Size = new System.Drawing.Size(880, 300);
+            this.dgvGV.TabIndex = 49;
+            this.dgvGV.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvGV_CellClick);
+            // 
+            // cb_mmh
+            // 
+            this.cb_mmh.FormattingEnabled = true;
+            this.cb_mmh.Location = new System.Drawing.Point(510, 115);
+            this.cb_mmh.Name = "cb_mmh";
+            this.cb_mmh.Size = new System.Drawing.Size(164, 21);
+            this.cb_mmh.TabIndex = 70;
+            this.cb_mmh.SelectedIndexChanged += new System.EventHandler(this.cb_mmh_SelectedIndexChanged);
+            // 
+            // frmGiaoVien
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(961, 654);
+            this.Controls.Add(this.cb_mmh);
+            this.Controls.Add(this.txtMaDT);
+            this.Controls.Add(this.lblMaDanToc);
+            this.Controls.Add(this.btnChiTiet);
+            this.Controls.Add(this.lblMaHS);
+            this.Controls.Add(this.txtTimKiem);
+            this.Controls.Add(this.lbTimkiem);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnSua);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.lblAddress);
+            this.Controls.Add(this.lblGender);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblTenHS);
+            this.Controls.Add(this.txtMaGV);
+            this.Controls.Add(this.lbTitle);
+            this.Controls.Add(this.dgvGV);
+            this.Name = "frmGiaoVien";
+            this.Text = "frmGiaoVien";
+            this.Load += new System.EventHandler(this.frmGiaoVien_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvGV)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtMaDT;
+        private System.Windows.Forms.Label lblMaDanToc;
+        private System.Windows.Forms.Button btnChiTiet;
+        private System.Windows.Forms.Label lblMaHS;
+        private System.Windows.Forms.TextBox txtTimKiem;
+        private System.Windows.Forms.Label lbTimkiem;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Label lblAddress;
+        private System.Windows.Forms.Label lblGender;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblTenHS;
+        private System.Windows.Forms.TextBox txtMaGV;
+        private System.Windows.Forms.Label lbTitle;
+        private System.Windows.Forms.DataGridView dgvGV;
+        private System.Windows.Forms.ComboBox cb_mmh;
+    }
+}
---
+++ CNPM/frmGiaoVien.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CNPM
+{
+    public partial class frmGiaoVien : Form
+    {
+        string connectionString = ConfigurationManager.ConnectionStrings["QLHS"].ConnectionString;
+        public frmGiaoVien()
+        {
+            InitializeComponent();
+        }
+
+
+        private void frmGiaoVien_Load(object sender, EventArgs e)
+        {
+           // InitializeComponent();
+            LoadData();
+            LoadMonHocToComboBox();
+        }
+        private void LoadData()
+        {
+            string query = "SELECT MaGiaoVien, TenGiaoVien, DiaChi, DienThoai, MaMonHoc FROM GIAOVIEN";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlDataAdapter adapter = new SqlDataAdapter(query, connection))
+                {
+                    DataTable table = new DataTable();
+                    adapter.Fill(table);
+                    dgvGV.DataSource = table;
+                }
+            }
+        }
+        public class ComboBoxItem
+        {
+            public string Value { get; set; }
+            public string Text { get; set; }
+
+            public ComboBoxItem(string value, string text)
+            {
+                Value = value;
+                Text = text;
+            }
+
+            public override string ToString()
+            {
+                return Text;
+            }
+        }
+        private void LoadMonHocToComboBox()
+        {
+          
+                string query = "SELECT MaMonHoc FROM MONHOC";
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand(query, connection);
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        cb_mmh.Items.Add(reader["MaMonHoc"].ToString());
+                    }
+                    reader.Close();
+                }
+            
+
+        }
+        private bool ValidateInput()
+        {
+            if (string.IsNullOrEmpty(txtMaGV.Text) ||
+                string.IsNullOrEmpty(txtName.Text) ||
+                string.IsNullOrEmpty(txtAddress.Text) ||
+                string.IsNullOrEmpty(txtMaDT.Text) ||
+                cb_mmh.SelectedItem == null)
+            {
+                MessageBox.Show("Vui lòng điền đầy đủ thông tin.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+
+            // Kiểm tra xem mã giáo viên đã tồn tại trong cơ sở dữ liệu chưa
+            string maGV = txtMaGV.Text;
+            string query = "SELECT COUNT(*) FROM GIAOVIEN WHERE MaGiaoVien = @MaGV";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@MaGV", maGV);
+                connection.Open();
+                int count = (int)command.ExecuteScalar();
+                if (count > 0)
+                {
+                    MessageBox.Show("Mã giáo viên đã tồn tại trong cơ sở dữ liệu.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (!ValidateInput())
+            {
+                return; // Nếu dữ liệu không hợp lệ, không thực hiện thêm và thoát khỏi hàm
+            }
+
+            // Lấy dữ liệu từ các TextBox và ComboBox
+            string maGV = txtMaGV.Text;
+            string tenGV = txtName.Text;
+            string diaChi = txtAddress.Text;
+            string dienThoai = txtMaDT.Text;
+            string maMonHoc = string.Empty;
+            object selectedItem = cb_mmh.SelectedItem;
+            if (selectedItem != null)
+            {
+                if (selectedItem is KeyValuePair<string, string>)
+                {
+                    KeyValuePair<string, string> selectedMonHoc = (KeyValuePair<string, string>)selectedItem;
+                    maMonHoc = selectedMonHoc.Key;
+                }
+                else if (selectedItem is string)
+                {
+                    maMonHoc = selectedItem.ToString();
+                }
+            }
+
+            // Thực hiện thêm dữ liệu vào cơ sở dữ liệu
+            string query = "INSERT INTO GIAOVIEN (MaGiaoVien, TenGiaoVien, DiaChi, DienThoai, MaMonHoc) VALUES (@MaGV, @TenGV, @DiaChi, @DienThoai, @MaMonHoc)";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@MaGV", maGV);
+                command.Parameters.AddWithValue("@TenGV", tenGV);
+                command.Parameters.AddWithValue("@DiaChi", diaChi);
+                command.Parameters.AddWithValue("@DienThoai", dienThoai);
+                command.Parameters.AddWithValue("@MaMonHoc", maMonHoc);
+
+                try
+                {
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    MessageBox.Show("Thêm giáo viên thành công.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    LoadData(); // Refresh DataGridView
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Đã xảy ra lỗi khi thêm giáo viên: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void cb_mmh_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dgvGV_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                // Lấy thông tin từ ô được chọn
+                DataGridViewRow row = dgvGV.Rows[e.RowIndex];
+                string maGV = row.Cells["MaGiaoVien"].Value.ToString();
+                string tenGV = row.Cells["TenGiaoVien"].Value.ToString();
+                string diaChi = row.Cells["DiaChi"].Value.ToString();
+                string dienThoai = row.Cells["DienThoai"].Value.ToString();
+                string maMonHoc = row.Cells["MaMonHoc"].Value.ToString();
+
+                // Hiển thị thông tin lên các TextBox và ComboBox
+                txtMaGV.Text = maGV;
+                txtName.Text = tenGV;
+                txtAddress.Text = diaChi;
+                txtMaDT.Text = dienThoai;
+
+                // Tìm và chọn môn học tương ứng trong ComboBox
+                foreach (var item in cb_mmh.Items)
+                {
+                    if (item is ComboBoxItem comboBoxItem)
+                    {
+                        if (comboBoxItem.Value == maMonHoc)
+                        {
+                            cb_mmh.SelectedItem = comboBoxItem;
+                            break;
+                        }
+                    }
+                }
+
+            }
+        }
+
+        private void UpdateTeacher(string maGV, string tenGV, string diaChi, string dienThoai, string maMonHoc)
+        {
+            string query = "UPDATE GIAOVIEN SET TenGiaoVien = @TenGV, DiaChi = @DiaChi, DienThoai = @DienThoai, MaMonHoc = @MaMonHoc WHERE MaGiaoVien = @MaGV";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@TenGV", tenGV);
+                command.Parameters.AddWithValue("@DiaChi", diaChi);
+                command.Parameters.AddWithValue("@DienThoai", dienThoai);
+                command.Parameters.AddWithValue("@MaMonHoc", maMonHoc);
+                command.Parameters.AddWithValue("@MaGV", maGV);
+
+                try
+                {
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    MessageBox.Show("Cập nhật thông tin giáo viên thành công.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    LoadData(); // Refresh DataGridView
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Đã xảy ra lỗi khi cập nhật thông tin giáo viên: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void DeleteTeacher(string maGV)
+        {
+            string query = "DELETE FROM GIAOVIEN WHERE MaGiaoVien = @MaGV";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@MaGV", maGV);
+
+                try
+                {
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    MessageBox.Show("Xóa giáo viên thành công.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    LoadData(); // Refresh DataGridView
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Đã xảy ra lỗi khi xóa giáo viên: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            // Kiểm tra dữ liệu đầu vào
+           
+
+            // Lấy thông tin từ các TextBox và ComboBox
+            string maGV = txtMaGV.Text;
+            string tenGV = txtName.Text;
+            string diaChi = txtAddress.Text;
+            string dienThoai = txtMaDT.Text;
+            string maMonHoc = cb_mmh.SelectedItem.ToString();
+
+            // Gọi hàm cập nhật giáo viên
+            UpdateTeacher(maGV, tenGV, diaChi, dienThoai, maMonHoc);
+
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            // Lấy mã giáo viên từ TextBox
+            string maGV = txtMaGV.Text;
+
+            // Gọi hàm xóa giáo viên
+            DeleteTeacher(maGV);
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            txtMaGV.Text = "";
+            txtName.Text = "";
+            txtAddress.Text = "";
+            txtMaDT.Text = "";
+            txtTimKiem.Text = "";
+            cb_mmh.Text = "";
+
+        }
+
+        private void txtTimKiem_TextChanged(object sender, EventArgs e)
+        {
+            // Lấy giá trị từ ô tìm kiếm
+            string keyword = txtTimKiem.Text.Trim();
+
+            // Thực hiện truy vấn tìm kiếm
+            string query = "SELECT MaGiaoVien, TenGiaoVien, DiaChi, DienThoai, MaMonHoc FROM GIAOVIEN WHERE TenGiaoVien LIKE @Keyword";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlDataAdapter adapter = new SqlDataAdapter(query, connection))
+                {
+                    adapter.SelectCommand.Parameters.AddWithValue("@Keyword", "%" + keyword + "%");
+
+                    DataTable table = new DataTable();
+                    adapter.Fill(table);
+                    dgvGV.DataSource = table;
+                }
+            }
+        }
+    }
+}
